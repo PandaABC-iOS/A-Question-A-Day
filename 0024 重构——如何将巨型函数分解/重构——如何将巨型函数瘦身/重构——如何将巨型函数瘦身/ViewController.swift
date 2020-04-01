339c55cfb6d585297559e321667d00757f79dea1
zjm <409089931@qq.com>
2020-04-01 09:02:33 +0800

    110范例1 无局部变量初始化

---
+++ 0024 重构——如何将巨型函数分解/重构——如何将巨型函数瘦身/重构——如何将巨型函数瘦身/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  重构——如何将巨型函数瘦身
+//
+//  Created by 张津铭 on 2020/4/1.
+//  Copyright © 2020 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 0024 重构——如何将巨型函数分解/重构——如何将巨型函数瘦身/重构——如何将巨型函数瘦身Tests/_____________Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _____________Tests.swift
+//  重构——如何将巨型函数瘦身Tests
+//
+//  Created by 张津铭 on 2020/4/1.
+//  Copyright © 2020 iOS. All rights reserved.
+//
+
+import XCTest
+@testable import _____________
+
+class _____________Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
