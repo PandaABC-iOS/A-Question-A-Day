b1726055a49997c085afd44bb32a14c1e5f41545
zjm <409089931@qq.com>
2020-04-14 09:34:28 +0800

    292 ReplaceParameterWithMethods 初始化

---
+++ 0031 /如何缩减过长的参数/如何缩减过长的参数/292ReplaceParameterWithMethods.swift
@@ -0,0 +1,50 @@
+//
+//  292ReplaceParameterWithMethods.swift
+//  Refactor
+//
+//  Created by 张津铭 on 2020/3/2.
+//  Copyright © 2020 Hangzhou. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 概述：
+ 对象调用某个函数，并将所得结果作为参数，传递给另一个函数。而接受该参数的函数自身也能够调用前一个函数。
+ 让参数接受者去除该项参数，并直接调用前一个函数。
+ 
+ 做法：
+ - 1. 如果有必要，将参数的计算过程提炼到一个独立函数中。
+ - 2. 将函数本体内引用该参数的地方改为调用新建的函数。
+ - 3. 每次替换后，修改并测试。
+ - 4. 全部替换完成后，使用 Remove Parameter将该参数去掉。
+ 
+ */
+class ReplaceParameterWithMethods {
+
+    private var _quantity = 0
+
+    private var _itemPrice = 0
+
+    func getPrice() -> Double {
+        let basePrice = _quantity * _itemPrice
+        let discountLevel: Int
+        
+        if _quantity > 100 {
+            discountLevel = 2
+        } else {
+            discountLevel = 1
+        }
+        
+        let finalPrice = discountedPrice(basePrice: basePrice, discountLevel: discountLevel)
+        return finalPrice
+    }
+    
+    func discountedPrice(basePrice: Int, discountLevel: Int) -> Double {
+        if discountLevel == 2 {
+            return Double(basePrice) * 0.1
+        } else {
+            return Double(basePrice) * 0.05
+        }
+    }
+}
